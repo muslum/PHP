563399ab402da4dfc86d92e60e9cac60ea826546
Muslum Dag <muslumdag@outlook.com>
2019-12-31 05:12:12 +0300

    Update rce2.php

--- rce2.php
+++ rce2.php
@@ -2,9 +2,9 @@
 error_reporting(0);
     $ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
     $ip = "8.8.8.8";
-    echo " hoşgeldin";
+    echo " merhaba";
     echo " $ip";
     echo "<pre>";
-    eval(system("curl https://ipinfo.ip/$ip > bilgi/bilgi.txt"));
+    eval(system("curl https://ipinfo.ip/$ip "));
     echo "</pre>";
 ?>
